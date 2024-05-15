2a64e80167e364c880ed69df46a9d63b0b1a5119
mohamedabdeldayem7 <mohamed.bk10150@gmail.com>
2024-05-16 01:15:58 +0300

    modify return type of methods

--- src/main/java/com/example/restdemo/controller/CloudVendorController.java
+++ src/main/java/com/example/restdemo/controller/CloudVendorController.java
@@ -32,20 +32,17 @@ public class CloudVendorController {
 
     @PostMapping
     public String createCloudVendor(@RequestBody CloudVendorDto cloudVendorDto) {
-        cloudVendorService.addVendor(cloudVendorDto);
-        return "add Vendor successfully";
+        return cloudVendorService.addVendor(cloudVendorDto);
     }
 
     @PutMapping
     public String updateCloudVendor(@RequestBody CloudVendorDto cloudVendorDto) {
-        cloudVendorService.updateVendor(cloudVendorDto);
-        return "update Vendor successfully";
+        return cloudVendorService.updateVendor(cloudVendorDto);
     }
 
     @DeleteMapping("/{id}")
     public String deleteCloudVendor(@PathVariable String id) {
-        cloudVendorService.deleteVendor(id);
-        return "Delete Vendor successfully";
+        return cloudVendorService.deleteVendor(id);
     }
 
 
