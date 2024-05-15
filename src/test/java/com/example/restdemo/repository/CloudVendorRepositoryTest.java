21cb5a5d5a16cb85ec5716cd0fc53a5d3890adbd
mohamedabdeldayem7 <mohamed.bk10150@gmail.com>
2024-05-15 05:59:49 +0300

    implementing setup and tearDown methods

--- src/test/java/com/example/restdemo/repository/CloudVendorRepositoryTest.java
+++ src/test/java/com/example/restdemo/repository/CloudVendorRepositoryTest.java
@@ -1,7 +1,33 @@
 package com.example.restdemo.repository;
 
+import com.example.restdemo.model.CloudVendor;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @DataJpaTest
 public class CloudVendorRepositoryTest {
+
+    @Autowired
+    CloudVendorRepository cloudVendorRepository;
+    CloudVendor cloudVendor;
+
+    @BeforeEach
+    void setUp() {
+        cloudVendor = CloudVendor.builder()
+                .vendorId("1")
+                .vendorName("Google vendor")
+                .vendorAddress("USA")
+                .vendorPhoneNumber("xxxxxxxxx")
+                .build();
+    }
+
+    @AfterEach
+    void tearDown() {
+        cloudVendor = null;
+        cloudVendorRepository.deleteAll();
+    }
+
+
 }
